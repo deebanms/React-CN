f4bf5af6c8bd3722b394ed0c34142d172fdd3b71
Deeban M S <deeban.ngl@gmail.com>
2024-11-04 23:35:24 +0530

    Solved react problems

---
+++ Basics/Problems/week 1/cra_q1_scaffold/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Write the message here. */}
+      <p>Welcome to React Course on CodingNinjas.com</p>
+    </div>
+  );
+}
+
+export default App;
