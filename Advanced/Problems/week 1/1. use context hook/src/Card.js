a491aa6c04c440b31c2b002154bbf2f06ab83206
Deeban M S <deeban.ngl@gmail.com>
2024-12-23 13:29:04 +0530

    Use context hook

---
+++ Advanced/Problems/week 1/1. use context hook/src/Card.js
@@ -0,0 +1,16 @@
+import { cardContext } from "./context";
+import { useContext } from "react";
+//Refactor this to use the useContext hook
+export const Card = () => {
+  const value = useContext(cardContext);
+  return (
+    <div className="card" style={{ backgroundColor: value.color }}>
+      <h3>{value.text}</h3>
+    </div>
+
+    // <cardContext.Consumer>
+    //   {(value) => {
+    //   }}
+    // </cardContext.Consumer>
+  );
+};
